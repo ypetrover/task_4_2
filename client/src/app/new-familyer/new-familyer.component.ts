ecd605f84b6b6df1ce52668c0c1135517ea2455d
ypetrover <47120377+ypetrover@users.noreply.github.com>
2019-10-23 20:30:04 +0300

    Update new-familyer.component.ts

--- client/src/app/new-familyer/new-familyer.component.ts
+++ client/src/app/new-familyer/new-familyer.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit, ElementRef, QueryList } from '@angular/core';
+import { FormBuilder, Validators, NgModel } from '@angular/forms';
 import { FamilyTasksService } from '../family-tasks.service';
 
 @Component({
@@ -11,27 +11,106 @@ export class NewFamilyerComponent implements OnInit {
 
   constructor(private service: FamilyTasksService, private fb: FormBuilder) { }
 
+  public familyName;
+  private ifEdit: Boolean = false;
+  public editedFam;
+  public editName;
+  public newFam;
+
   ngOnInit() {
+    this.service.allFamily().subscribe(
+      res => this.familyName = res,
+      err => console.log(err)
+    )
   }
 
-  public newFam;
   famForm = this.fb.group({
-    name: [''],
+    name: ['', Validators.required],
     nik: [''],
     description: ['']
   });
+
+  editFam = this.fb.group({
+    name: ['', Validators.required],
+    nik: [''],
+    description: ['']
+  });
+
 
   addFam() {
     this.service.newFam(this.famForm.value).subscribe(
       res => {
         this.newFam = res
         this.famForm = this.fb.group({
-          name: [''],
+          name: ['', Validators.required],
           nik: [''],
           description: ['']
         })
+
+        const saveUser = () => {
+          if (this.famForm.dirty && this.famForm.valid) {
+            alert(`Name: ${this.famForm.value.name} nik: ${this.famForm.value.nik}`);
+          }
+        }
+
+        this.service.allFamily().subscribe(
+          res => this.familyName = res,
+          err => console.log(err)
+        )
+        console.log('new familyer added')
       },
       err => console.log(this.newFam.value, err)
     );
   }
+
+
+  deleteFam(name) {
+    this.service.deleteFam(name.value).subscribe(
+      res => {
+        console.log(name.value, 'is deleted')
+        this.service.allFamily().subscribe(
+          res => this.familyName = res,
+          err => console.log(err)
+        )
+      },
+      err => console.log(err)
+    )
+  }
+
+  editFalse(name) {
+    console.log(name, name.value)
+    this.service.oneFamily(name.value).subscribe(
+      res => {
+        let data: any = res
+        this.editFam = this.fb.group({
+          name: [data.name],
+          nik: [data.nik],
+          description: [data.description]
+        });
+      },
+      err => console.log(err)
+    )
+    this.ifEdit = true;
+  }
+
+  name(event) {
+    this.editName = event.target.value
+  }
+
+  edit() {
+    const d = { name: this.editName, data: this.editFam.value }
+    this.ifEdit = false;
+    this.service.editFam(d).subscribe(
+      res => {
+        this.service.allFamily().subscribe(
+          res => this.familyName = res,
+          err => console.log(err)
+        )
+        console.log('the familyer edited')
+      },
+      err => console.log(err)
+    )
+  }
+
+
 }
