7b71732c2507bc81721d9fd0cbd68e8877ed11ca
ypetrover <47120377+ypetrover@users.noreply.github.com>
2019-10-23 19:58:33 +0300

    Update heandlers.js

--- server/heandlers/heandlers.js
+++ server/heandlers/heandlers.js
@@ -42,4 +42,38 @@ const deleteTask = (req, res) => {
     })
 }
 
-module.exports = { allFamily, allTasks, newTask, newFam, deleteTask }
+const delFam = (req, res) => {
+    let name = req.params.name
+    famileyModel.deleteOne({ name: name }, (err) => {
+        if (err) throw err
+        res.json('deleted')
+    })
+}
+
+const editFam = (req, res) => {
+    let name = req.params.name
+    famileyModel.find({ name: name }, { _id: 0, __v: 0 }, (err, data) => {
+        if (err) throw err
+        let name = req.params.name
+        let newName = req.query.name
+        let nik = req.query.nik
+        let description = req.query.desc
+        console.log(req.query)
+        famileyModel.updateOne({ name: name }, { name: newName, nik: nik, description: description }, (err, data) => {
+            if (err) throw err
+            console.log(name, nik, description)
+            console.log('updated', data.n, 'document')
+        })
+        res.json(data)
+    })
+}
+
+const oneFamily = (req, res) => {
+    console.log(req.params)
+    famileyModel.find({ name: req.params.name }, {}, (err, data) => {
+        if (err) throw err
+        res.json(data[0])
+    })
+}
+
+module.exports = { allFamily, allTasks, newTask, newFam, deleteTask, delFam, editFam, oneFamily }
